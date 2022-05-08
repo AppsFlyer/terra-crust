595b53cb3fb7a8bc1c57c3e048a7af95dff055b2
osher.elmakaies <osher.elmakaies@appsflyer.com>
2022-05-08 12:11:55 +0300

    feat: Added custom template for main

--- internal/cmd/app/generate_main.go
+++ internal/cmd/app/generate_main.go
@@ -16,7 +16,7 @@ func generateMain(root *RootCommand) *cobra.Command {
 
 			terraformSvc := InitTerraformGeneratorService(log)
 
-			if err := terraformSvc.GenerateMain(flags.SourcePath, flags.DestinationPath); err != nil {
+			if err := terraformSvc.GenerateMain(flags.SourcePath, flags.DestinationPath, flags.MainTemplateFilePath); err != nil {
 				log.ErrorWithError("Failed generating the terraform main file", err)
 
 				return err
@@ -28,6 +28,7 @@ func generateMain(root *RootCommand) *cobra.Command {
 
 	cmd.Flags().StringVar(&flags.SourcePath, "source-path", "", "Required: General module folder path that contains all the sub modules flattened")
 	cmd.Flags().StringVar(&flags.DestinationPath, "destination-path", "", "Required: Destination path to write the new terraform file")
+	cmd.Flags().StringVar(&flags.MainTemplateFilePath, "main-template-path", "", "Optional: Custom main template path for generated module, will take default if not provided")
 	if err := cmd.MarkFlagRequired("source-path"); err != nil {
 		root.log.ErrorWithError("failed to set required flag on source-path", err)
 	}
