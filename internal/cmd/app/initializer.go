de2b9723d67479f2bc328f5c1ba670fa0e475233
osher.elmakaies <osher.elmakaies@appsflyer.com>
2022-06-22 19:46:21 +0300

    Revert "fix: fixing the need for config folder , added the variables as consts"
    This reverts commit fb7caee8c61e6a0b24bbabd9c983bba25a1bc1d5.

--- internal/cmd/app/initializer.go
+++ internal/cmd/app/initializer.go
@@ -6,15 +6,13 @@ import (
 	"github.com/AppsFlyer/terra-crust/internal/services/drivers"
 )
 
-const LOCALS_TEMPLATE_PATH = "templates/locals_file.tmpl"
-const VARIABLE_TEMPLATE_PATH = "templates/variables_file.tmpl"
-const MAIN_TEMPLATE_PATH = "templates/main_file.tmpl"
-
 func InitTerraformGeneratorService(log logger.Logger) *services.Terraform {
+	config := NewConfig(log)
+
 	parserDriver := drivers.NewTerraformParser(log)
 	parser := services.NewParser(log, parserDriver)
 	templateHandler := services.NewTemplateHandler(log)
-	tfSvc := services.NewTerraform(log, parser, templateHandler, LOCALS_TEMPLATE_PATH, VARIABLE_TEMPLATE_PATH, MAIN_TEMPLATE_PATH)
+	tfSvc := services.NewTerraform(log, parser, templateHandler, config.GetString("LOCALS_TEMPLATE_PATH"), config.GetString("VARIABLE_TEMPLATE_PATH"), config.GetString("MAIN_TEMPLATE_PATH"))
 
 	return tfSvc
 }
