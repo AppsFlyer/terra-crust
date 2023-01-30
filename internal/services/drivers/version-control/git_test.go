9f9e86bb3165c8b7dd272e7c5e5d82ed9cc267a5
osher.elmakaies <osher.elmakaies@appsflyer.com>
2023-01-30 15:07:05 +0200

    fix(RemoteModules): added more tests
    Signed-off-by: osher.elmakaies <osher.elmakaies@appsflyer.com>

--- internal/services/drivers/version-control/git_test.go
+++ internal/services/drivers/version-control/git_test.go
@@ -9,13 +9,29 @@ import (
 
 const (
 	TerraCrustModuleName = "terracrust"
+	NamigModuleName      = "terraform-aws-resource-naming"
+	ZonesModuleName      = "zones"
 	TerraCrustURL        = "https://github.com/AppsFlyer/terra-crust"
-	ModulesTestPath      = "./temp-git-test"
+
+	ModulesTestPath = "./temp-git-test"
 )
 
 var mockModules = map[string]*version_control.RemoteModule{
 	TerraCrustModuleName: {
 		Url: TerraCrustURL,
+	},
+	NamigModuleName: {
+		Url:     "https://github.com/traveloka/terraform-aws-resource-naming",
+		Version: "v0.23.1",
+	},
+	ZonesModuleName: {
+		Url:  "https://github.com/terraform-aws-modules/terraform-aws-route53.git",
+		Path: "modules/zones",
+	},
+}
+var mockBadModules = map[string]*version_control.RemoteModule{
+	TerraCrustModuleName: {
+		Url: "https://github.com/appsflyer/terra-crust/test/bad",
 	},
 }
 
@@ -39,8 +55,16 @@ func TestCloneAndCleanup(t *testing.T) {
 		t.Errorf("Clone failed , did not find terra-crust")
 	}
 
-	if len(folders) != 1 {
-		t.Errorf("Expected 1 folder count received %d", len(folders))
+	if _, exist := folders[ZonesModuleName]; !exist {
+		t.Errorf("Clone failed , did not find zones")
+	}
+
+	if _, exist := folders[NamigModuleName]; !exist {
+		t.Errorf("Clone failed , did not find naming")
+	}
+
+	if len(folders) != 3 {
+		t.Errorf("Expected 3 folder count received %d", len(folders))
 	}
 
 	if err = gitDriver.CleanModulesFolders(mockModules, ModulesTestPath); err != nil {
@@ -56,8 +80,27 @@ func TestCloneAndCleanup(t *testing.T) {
 		t.Errorf("cleanup-failed terracrust folder still exists")
 	}
 
+	if _, exist := folders[ZonesModuleName]; exist {
+		t.Errorf("cleanup-failed zones folder still exists")
+	}
+
+	if _, exist := folders[NamigModuleName]; exist {
+		t.Errorf("cleanup-failed naming folder still exists")
+	}
+
 	if len(folders) != 0 {
 		t.Errorf("Expected 0 folder count received %d", len(folders))
+	}
+}
+
+func TestFailBadUrl(t *testing.T) {
+	t.Parallel()
+	log := logger.NewSimple()
+	gitDriver := version_control.InitGitProvider(log)
+
+	err := gitDriver.CloneModules(mockBadModules, ModulesTestPath)
+	if err == nil {
+		t.Errorf("expected error received error nil")
 	}
 }
 
